fa68667d83b4f43a083381988a7256f511dd67d6
Andrey-8 <ademirus1@gmail.com>
2025-01-09 18:12:09 +0500

    Update probnik.py

--- probnik.py
+++ probnik.py
@@ -1,23 +1,53 @@
-#для модуля 5, задача 1. 
-#Домашняя работа по уроку "Атрибуты и методы объекта"
-
-class House:
+class Vehicle:
     
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    
+    
+    def __init__(self, owner, __model, __engine_power, __color):
+    
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+    
+    def get_model(self):
+        print (f'Модель: {self.__model}')
         
+    def get_horsepower(self):
+        print (f'Мощность двигателя: {self.__engine_power}')
         
-    def go_to(self, new_floor):
-        if new_floor <= self.number_of_floors >1:
-            for floor in range(1, new_floor + 1):
-                print(floor)
+    def get_color (self):
+        print (f'Цвет: {self.__color}')
+        
+    def print_info(self):
+        Vehicle.get_model(self)
+        Vehicle.get_horsepower(self)
+        Vehicle.get_color(self)
+        print(f'Владелец: {self.owner}')
+        
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
-            print('Такого этажа не существует')
+            print(f'Нельзя поменять цвет на {new_color}')
             
-        
-                
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+            
+    
+class Sedan(Vehicle):
+    
+    __PASSENGERS_LIMIT = 5
+    
+    
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
