501b10f54e09bcbbc61e85848ee823daba4086d5
Ademirus <ademirus1@gmail.com>
2025-01-27 18:03:01 +0500

    временные

--- probnie.py
+++ probnie.py
@@ -1,12 +1,52 @@
-def fake_divide(first, second):
-    rez = first / second
-    if second == 0:
-        return False
+class WordsFinder:
+    def __init__(self, *file_name):
+        self.file_names = [*file_name]
+        self.file_name = file_name
 
-result1 = fake_divide(69, 3)
+    @property
+    def get_all_words(self):
+        all_words = {}
+        words = []
+        str_punctuation = [',', '.', '=', '!', '?', ';', ':', '-']
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as open_file:
+                for line in open_file:
+                    line = line.lower()
+                    for p in str_punctuation:
+                        if p in line:
+                            line = line.replace(p, '')
+                    split_line = line.split(sep=' ')
+                    words.append(split_line)
+        sorted_list = [x for y in words for x in y]
+        all_words[self.file_name] = sorted_list
+        return all_words
 
-result2 = fake_divide(3, 0)
+    def find(self, word):
+        dict_ = self.get_all_words
+        list_ = []
+        for name, words in dict_.items():
+            for w in words:
+                if word.lower() in w:
+                    index = words.index(w)
+                    list_.append(self.file_name)
+                    list_.append(index + 1)
+                    break
+        return list_
 
-print(result1)
+    def count(self, word):
+        dict_ = self.get_all_words
+        list_ = []
+        count = 0
+        for name, words in dict_.items():
+            for w in words:
+                if word.lower() in w:
+                    count += 1
+        list_.append(self.file_name)
+        list_.append(count)
+        return list_
 
-print(result2)
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words)
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
