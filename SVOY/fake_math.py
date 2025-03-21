61a531ac8a2d7f95ad1fd9138adb2bbb46486811
Andrey-8 <ademirus1@gmail.com>
2025-03-21 13:19:48 +0500

    Add files via upload

---
+++ SVOY/fake_math.py
@@ -0,0 +1,12 @@
+def dividef(first, second):
+    rez = first / second
+    if second != 0:
+        return rez
+    else: False
+
+
+result3 = dividef(49, 7)
+result4 = dividef(15, 0)
+
+print(result3)
+print(result4)
