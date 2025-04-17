b6d47bb459c269879740a552932c6169973779bc
Andrey-8 <ademirus1@gmail.com>
2025-04-17 13:48:05 +0500

    Add files via upload

---
+++ SVOY/угадайка.py
@@ -0,0 +1,19 @@
+import random
+from random import choice
+
+print('проверь свою интуицию')
+s = [0,1,2,4,5,3,6,7,8,9,10]
+f = True
+while f:
+    d = int(input("введи цифру от 0 до 10: "))
+    if 10 >= d >= 0:
+        w = choice(s)
+        print(w)
+    else:
+        f = False
+        print("КОНЕЦ ИГРЫ")
+    if w == d:
+        f = False
+        print('УГАДАЛ!!!, конец игры')
+
+
